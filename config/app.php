e16f66e3b99cd65f75fe234d15bad7bf8427f805
Dustin Cohenour <dustincohenour@gmail.com>
2016-09-10 04:31:12 -0400

    Socialite Configuration
    - Setup laravel/socialite service provider and alias in config/app.php

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
          * Third-Party Service Providers...
          */
         Laravel\Scout\ScoutServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
 
     ],
 
@@ -229,6 +230,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Third-Party Aliases
+         */
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
